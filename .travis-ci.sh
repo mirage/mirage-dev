d9e287886ee885555b46f6193cb256e76bd154df
Anil Madhavapeddy <anil@recoil.org>
2014-10-04 17:56:54 +0100

    also install github in travis

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -17,7 +17,7 @@ export OPAMYES=1
 opam init git://github.com/ocaml/opam-repository >/dev/null 2>&1
 opam repo add mirage-dev .
 
-TARGETS="mirage-http mirage mirage-xen"
+TARGETS="mirage-http mirage mirage-xen github"
 opam install ${TARGETS}
 eval `opam config env`
 git clone -b mirage2 git://github.com/avsm/mirage-skeleton
