3c148d8a4913a57c04e4c2bf044c7d64ad504f51
Anil Madhavapeddy <anil@recoil.org>
2014-10-13 00:45:57 +0100

    travis: update packages after adding remote

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -16,6 +16,7 @@ export OPAMYES=1
 
 opam init git://github.com/ocaml/opam-repository >/dev/null 2>&1
 opam repo add mirage-dev .
+opam update -u
 
 TARGETS="mirage-http mirage mirage-xen github"
 opam install ${TARGETS}
