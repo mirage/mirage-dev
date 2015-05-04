c2500ca0c7a6a01cf94bf2b685837ab192a9baad
Hannes Mehnert <hannes@mehnert.org>
2015-05-04 16:52:34 +0200

    revert change to build gmp only for xen

--- packages/gmp-xen/gmp-xen.6.0.0/files/mirage-install.sh
+++ packages/gmp-xen/gmp-xen.6.0.0/files/mirage-install.sh
@@ -3,8 +3,12 @@ PREFIX=`opam config var prefix`
 PKG_CONFIG_PATH="$PREFIX/lib/pkgconfig"
 export PKG_CONFIG_PATH
 
+make install -C unix
 mkdir -p "$PREFIX/lib/pkgconfig"
 cp gmp-xen.pc "$PREFIX/lib/pkgconfig/"
 
-ocamlfind install gmp-xen META
-cp .libs/libgmp.a "$PREFIX/lib/gmp/libgmp_xen.a"
+ocamlfind install gmp META
+
+if pkg-config --exists mirage-xen; then
+  cp xen/.libs/libgmp.a "$PREFIX/lib/gmp-xen/libgmp_xen.a"
+fi
