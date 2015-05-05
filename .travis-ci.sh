25c2600d0794eea32ee916fa2b84c9b0acd13ebf
Hannes Mehnert <hannes@mehnert.org>
2015-05-05 22:11:49 +0200

    install fresh gcc and binutils

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -9,6 +9,14 @@ echo "yes" | sudo add-apt-repository ppa:$ppa
 sudo apt-get update -qq
 sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam time libgmp-dev
 
+sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+sudo apt-get -qq update
+sudo apt-get -qq install gcc-4.8
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 90
+
+wget http://mirrors.kernel.org/ubuntu/pool/main/b/binutils/binutils_2.24-5ubuntu3.1_amd64.deb
+sudo dpkg -i binutils_2.24-5ubuntu3.1_amd64.deb
+
 echo OCaml version
 ocaml -version
 
