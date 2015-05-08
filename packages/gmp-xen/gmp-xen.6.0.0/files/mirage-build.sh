11deb4fd995c6a4f448832f1b9e73788902f87b8
Hannes Mehnert <hannes@mehnert.org>
2015-05-08 14:01:26 +0200

    depend on mirage-xen-posix, instead of mirage-xen. the latter depends on cstruct etc., whereas gmp-xen only needs some CFLAGS and include files. minimizes recompilations

--- packages/gmp-xen/gmp-xen.6.0.0/files/mirage-build.sh
+++ packages/gmp-xen/gmp-xen.6.0.0/files/mirage-build.sh
@@ -3,7 +3,7 @@ PREFIX=`opam config var prefix`
 PKG_CONFIG_PATH="$PREFIX/lib/pkgconfig"
 export PKG_CONFIG_PATH
 
-CPPFLAGS="$CPPFLAGS `pkg-config mirage-xen --cflags` -O2 -pedantic -fomit-frame-pointer -fno-builtin -D_FORTIFY_SOURCE=0 -Wmissing-prototypes --std=gnu99"
+CPPFLAGS="$CPPFLAGS `pkg-config mirage-xen-posix --cflags` -O2 -pedantic -fomit-frame-pointer -fno-builtin -D_FORTIFY_SOURCE=0 -Wmissing-prototypes --std=gnu99"
 # Use different --host and --build to trigger cross-compilation mode (don't try to test binaries during configure)
 # Set CC to stop it trying to find a separate compiler for HOST.
 # Pass CPPFLAGS (not just CFLAGS) to stop it finding the Linux headers (just generates warnings).
