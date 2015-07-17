1fcac4a9fffdf956d9d74ef74711bba6a1aeb78b
Thomas Gazagnaire <thomas@gazagnaire.org>
2015-07-17 12:20:18 -0700

    More test fixes

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -40,9 +40,9 @@ make build MODE=xen
 #### TMP: irmin + xen
 
 git clone git://github.com/samoht/mirmin
-cd mirmin/git && ( \
-    mirage configure --xen & \
+(cd mirmin/git && \
+    mirage configure --xen && \
     make )
-cd mirmin/irmin && ( \
-    mirage configure --xen & \
+(cd mirmin/irmin && \
+    mirage configure --xen && \
     make )
