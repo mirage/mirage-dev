df931ab17b598481d9e3625fc3fa0ad8370e18e1
Hannes Mehnert <hannes@mehnert.org>
2016-06-29 17:13:53 +0100

    need some trusty

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -8,10 +8,13 @@ echo "yes" | sudo add-apt-repository ppa:$ppa
 sudo apt-get update -qq
 sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam time libgmp-dev
 
+TRUSTY="deb mirror://mirrors.ubuntu.com/mirrors.txt trusty main restricted universe"
+sudo add-apt-repository "${TRUSTY}"
 sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 sudo apt-get -qq update
 sudo apt-get -qq install gcc-4.8
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 90
+sudo add-apt-repository -r "${TRUSTY}"
 
 echo OCaml version
 ocaml -version
