94d3890b0b02f41063c6d46f5a769a392a6b5003
Mindy Preston <mindy.preston@docker.com>
2017-02-27 07:30:19 -0600

    build the default branch of mirage-skeleton for tests

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -7,7 +7,7 @@ opam repo add mirage-dev .
 opam update -u
 opam install mirage
 
-git clone -b mirage-dev git://github.com/mirage/mirage-skeleton
+git clone git://github.com/mirage/mirage-skeleton
 cd mirage-skeleton
 make MODE=${MODE}
 
