ab319670a3756bab279dc8e8fd919c84ae666cd6
Anil Madhavapeddy <anil@recoil.org>
2017-06-26 20:04:08 +0100

    activate tests

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -5,6 +5,7 @@ eval $(opam config env)
 
 opam repo add mirage-dev .
 opam update -u
+export OPAMBUILDTEST=1
 opam depext -j2 -uiy mirage mirage-dns mirage-conduit conduit-async conduit-lwt-unix tcpip
 
 #git clone git://github.com/mirage/mirage-skeleton
