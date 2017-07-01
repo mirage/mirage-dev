e89ec5d3987c2f747fc4a51ca225b74b41f2f793
Anil Madhavapeddy <anil@recoil.org>
2017-07-01 19:23:26 -0400

    build cohttp in tests

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -6,7 +6,7 @@ eval $(opam config env)
 opam repo add mirage-dev .
 opam update -u
 export OPAMBUILDTEST=1
-opam depext -j2 -uiy mirage mirage-dns mirage-conduit conduit-async conduit-lwt-unix tcpip
+opam depext -j2 -uiy mirage mirage-dns mirage-conduit conduit-async conduit-lwt-unix tcpip cohttp-async cohttp-lwt-jsoo cohttp-lwt-unix cohttp-top
 
 #git clone git://github.com/mirage/mirage-skeleton
 #cd mirage-skeleton
