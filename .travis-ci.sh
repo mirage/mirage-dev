017cca529c0394a546ff3fbd5402b479ee90be80
Hannes Mehnert <hannes@mehnert.org>
2018-11-07 01:34:37 +0100

    test skeleton in travis

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -5,10 +5,10 @@ eval $(opam config env)
 
 opam repo add mirage-dev .
 opam update -u
-export OPAMBUILDTEST=1
-opam depext -j2 -uiy mirage mirage-dns mirage-conduit conduit-async conduit-lwt-unix tcpip cohttp-async cohttp-lwt-jsoo cohttp-lwt-unix cohttp-top
 
-#git clone git://github.com/mirage/mirage-skeleton
-#cd mirage-skeleton
-#make MODE=${MODE}
+opam depext -uiy mirage
+
+git clone git://github.com/mirage/mirage-skeleton
+cd mirage-skeleton
+make MODE=${MODE}
 
